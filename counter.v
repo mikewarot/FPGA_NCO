56015c4bb00f137f278763aeeafcbcac2e6d029c
Mike Warot <chezmike@gmail.com>
2024-02-07 20:00:38 -0600

    streamline code

--- counter.v
+++ counter.v
@@ -8,19 +8,15 @@ module top
     );
 
 localparam MAXBIT = 31; 
-reg [5:0] ledCounter = 0;
 reg [MAXBIT:0] phaseAccumulator = 0;
 reg [MAXBIT:0] phaseIncrement = 1;
 reg [MAXBIT:0] increment_next = 1;
-reg [MAXBIT:0] aa,bb,cc,carry = 0;
-reg [MAXBIT:0] newcarry = 0;
-
-reg [MAXBIT:0] sum = 0;
+reg [MAXBIT:0] aa,bb,cc,carry,newcarry,sum;
 
 reg last_btn1 = 0;
 reg last_btn2 = 0;
 
-wire out_clk;
+wire clk_out;
 wire clk_lock;
 
 
@@ -31,13 +27,14 @@ rPLL #( // For GW1NR-9C C6/I5 (Tang Nano 9K proto dev board)
   .ODIV_SEL(4) // -> VCO = 799.2 MHz (range: 400-1200 MHz)
 ) pll (.CLKOUTP(), .CLKOUTD(), .CLKOUTD3(), .RESET(1'b0), .RESET_P(1'b0), .CLKFB(1'b0), .FBDSEL(6'b0), .IDSEL(6'b0), .ODSEL(6'b0), .PSDA(4'b0), .DUTYDA(4'b0), .FDLY(4'b0),
   .CLKIN(in_clk), // 27 MHz
-  .CLKOUT(out_clk), // 199.8 MHz
+  .CLKOUT(clk_out), // 199.8 MHz
   .LOCK(clk_lock)
 );
 
-always@(posedge out_clk) begin
+always@(posedge clk_out) begin
     carry <= newcarry << 1;
     phaseAccumulator <= sum;
+    phaseIncrement <= increment_next;
 end;
 
 always @* begin
@@ -65,11 +62,7 @@ always@(posedge in_clk) begin
   last_btn2 <= btn2;
 end
 
-// D flip-flop for phaseIncrement
-always @(posedge in_clk) begin
-  phaseIncrement <= increment_next;
-end
-
+// Output logic
 assign led = ~phaseAccumulator[MAXBIT:MAXBIT-5];
 assign f_out = phaseAccumulator[MAXBIT];
 
